2c86af1dcbb335c6509c8f1ef688acc5594ac744
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-06-23 14:38:26 -0500

    Summary
    Summary

---
+++ public/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";(new class{saludo(){console.log("Hola mundo")}}).saludo()})();
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+//Web
+
+//npm init
+//npm i webpack webpack-cli ts-loader typescript -D
+
+class Persona {
+    saludo(){
+        console.log("Hola mundo")
+    }
+}
+
+const persona1 = new Persona;
+
+persona1.saludo();
